4b8bd3766e8cf7618eef7a78a438b982eb4d5713
Nguyễn Văn Quang <quangnvgch211372@fpt.edu.vn>
2024-01-20 23:12:08 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button @click="loginWithGoogle">Login</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+  methods: {
+    loginWithGoogle() {
+      axios
+        .get("http://127.0.0.1:8000/api/auth/google")
+        .then((response) => {
+          const redirectUrl = response.data.url;
+          if (redirectUrl) {
+  
+            window.location.href = redirectUrl;
+          } else {
+            console.error("Unexpected response format:", response.data);
+          }
+        })
+        .catch((error) => {
+          console.error("Error making the request:", error);
+        });
+    },
+  },
+};
+</script>
